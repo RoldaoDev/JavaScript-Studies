f90774e82592c26db8464880d20ae431faedf269
Cauan Roldao <cauanroldao1503@gmail.com>
2025-03-19 09:29:07 -0300

    nada

--- Aula_prática_1/script.js
+++ Aula_prática_1/script.js
@@ -4,13 +4,13 @@ function gerarTabuada() {
 
     resultadoDiv.innerHTML = "";
     if (numero < 1 || numero > 10 || isNaN(numero)) {
-        resultadoDiv.innerHTML = '<p style='color: red'> Por favor insira um número entre 1 e 10.</p>';
+        resultadoDiv.innerHTML = '<p> Por favor insira um número entre 1 e 10.</p>';
         return;
     }
 
     let tabuada = '<h2> Tabuada do + numero </h2>';
     for (let i = 1; i <= 10; i++){
-        tabuada += '<p>${numero} x ${i} = ${numero * i}</p>';
+        tabuada += <p>'${numero}' x ${i} = ${numero * i}</p>;
     }
 
     resultadoDiv.innerHTML = tabuada;
